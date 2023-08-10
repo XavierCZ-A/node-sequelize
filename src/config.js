7a09fc171daeae67e541b3056dfeb85537278d5b
Xavier <xavierkta125@gmail.com>
2023-08-09 23:33:02 -0600

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-export const DB_HOST = process.env.DB_HOST || "localhost";
+export const DB_HOST = process.env.DB_HOST || "postgresdb";
 export const DB_USER = process.env.DB_USER || "postgres";
 export const DB_PASSWORD = process.env.DB_PASSWORD || "mysecretpassword";
 export const DB_DATABASE = process.env.DB_DATABASE || "investments";
